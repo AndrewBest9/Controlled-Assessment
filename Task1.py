66e26fe58d5b1827703c663dd8cd3868a04671b6
SexyBeast69 <apbest12345@gmail.com>
2014-06-18 09:29:06 +0100

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,16 @@
+def  
+    symbols = (GBP, EUR, USD,JPY)
+    money rates = [1,2,3,4]
+print choice of rate 
+
+if choice == money rates 
+          entry = input ("selected GBP")
+          
+if choice == money rates 
+          entry = input ("selected EUR")
+          
+if choice == money rates 
+          entry = input ("selected USD")
+  
+if choice == money rates 
+          entry = input ("selected JPY")
