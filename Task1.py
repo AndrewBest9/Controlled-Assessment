a71b7097976fa1db3dec839640e9806357f78a3e
SexyBeast69 <apbest12345@gmail.com>
2014-06-18 09:55:36 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -5,12 +5,13 @@ print choice of rate
 
 if choice == money rates 
           entry = input ("selected GBP")
-          
+          convert "GBP" to "EUR", "USD" and "JPY" if chosen  
 if choice == money rates 
           entry = input ("selected EUR")
-          
+          convert "EUR" to "GBP", "USD" and "JPY" if chosen
 if choice == money rates 
           entry = input ("selected USD")
-  
+          convert "USD" to "GBP", "EUR" and "JPY" if chosen
 if choice == money rates 
           entry = input ("selected JPY")
+          convert "JPY" to "GBP", "EUR" and "USD" if chosen
